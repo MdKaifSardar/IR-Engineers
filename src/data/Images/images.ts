d7eab37dbc10e801afe44314aad1369298c675b8
Md Kaif Sardar <mkaifsard564773@gmail.com>
2024-11-13 18:33:42 +0530

    bug fix attempt

--- src/data/Images/images.ts
+++ src/data/Images/images.ts
@@ -3,7 +3,7 @@ import AboutSide2 from "../../assets/Images/AbtDivSd2.png"
 import OurService from "../../assets/Images/OurServices.png"
 import WhChU1 from "../../assets/Images/WhChU1.png"
 import WhChU2 from "../../assets/Images/WhChU2.png"
-import logo from "../../assets/Logos/image1.png"
+import logo from "../../assets/Logos/LogoImage.png"
 
 export {
     AboutSide1, AboutSide2, OurService, WhChU1, WhChU2, logo
