98f2e2ad6d9d16a711a5a906658eb5d7d6921d3d
Md Kaif Sardar <mkaifsard564773@gmail.com>
2024-11-15 12:53:49 +0530

    fixed some bugs

--- src/components/Hero/HeroText.tsx
+++ src/components/Hero/HeroText.tsx
@@ -6,14 +6,14 @@ const HeroText = () => {
       <div className="mb-1 px-5 md:px-10 md:pb-4 pb-2 font-bold border-white border-b-[1px] md:text-5xl text-3xl text-white">
         {MainHeroText.title}
       </div>
-      <div className="mb-3 gap-1 md:gap-3 flex flex-row justify-center items-center">
+      <div className="mb-3 gap-2 flex flex-row justify-center items-center">
         {MainHeroText.bottomText.map((text, index) => (
-          <div key={index}>
+          <div key={index} className="flex flex-row justify-center items-center gap-2">
             <div className="lg:text-2xl text-sm text-white">
               {text}
             </div>
             {!(index == 2) ? (
-              <div className="text-2xl rounded-full h-1 w-1 bg-white" />
+              <div className="text-2xl rounded-full h-1 md:h-2 w-1 md:w-2 bg-white" />
             ) : null}
           </div>
         ))}
