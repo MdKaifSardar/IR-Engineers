ed9a77c2ab01f2ae3717b0304ba93832b9bdb422
Md Kaif Sardar <mkaifsard564773@gmail.com>
2024-11-16 21:54:07 +0530

    added the contact Poput functionality

--- src/components/SidePopup/SidePopup.tsx
+++ src/components/SidePopup/SidePopup.tsx
@@ -2,11 +2,22 @@ import { Link } from "react-router-dom"; // Import Link from react-router-dom
 import { sidePopup } from "../../data/Images/images";
 
 const SidePopupComponent = () => {
+  const handleClick = () => {
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    if (isMobile) {
+      // Open dial pad for mobile devices
+      window.location.href = "tel:+918910001691";
+    } else {
+      // Open WhatsApp chat for desktop devices
+      window.open("https://wa.me/918910001691", "_blank");
+    }
+  };
+
   return (
     <Link
       to="#"
       className="h-[4%] sm:h-[5%] z-[4] fixed right-0 top-[50%]"
-      onClick={() => (window.location.href = "tel:+918910001691")}
+      onClick={handleClick}
     >
       <img
         src={sidePopup}
